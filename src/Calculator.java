891b4f89d834077aec2e4487c0690625561ec0fe
betsylouj <betsylouj@gmail.com>
2015-07-20 18:15:04 -0700

    Simon Says Recipe

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,30 @@
 
 public class Calculator {
 
+	
+	public static void main(String[] args) {
+		new Calculator().go();
+	}
+
+	private void go() {
+		
+		
+	}
+	
+	public double add(double a, double b){
+		return a+b;
+	}
+	public double minus(double a, double b){
+		return a-b;
+	}
+	public double multiply(double a, double b){
+		return a*b;
+	}
+
+	public double divide(double a, double b){
+		return a/b;
+	}
+	public double exp(double a, double b){
+		return Math.pow(a, b);
+	}
 }
